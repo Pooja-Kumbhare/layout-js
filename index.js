efe342c69e2d3d97322edc641791fb27d9eb4a81
Pooja-Kumbhare <109602052+Pooja-Kumbhare@users.noreply.github.com>
2024-09-06 15:23:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+let inputEl = document.getElementById("demo");
+
+function onGreen(){
+ inputEl.style.backgroundColor ="green";
+ console.log(inputEl)
+ 
+}
+function onBlue(){
+    inputEl.style.backgroundColor= "blue";
+}
+
+function onYellow(){
+    inputEl.style.backgroundColor = "yellow";
+}
+
+function onSkyblue(){
+    inputEl.style.backgroundColor = "skyblue";
+}
+
